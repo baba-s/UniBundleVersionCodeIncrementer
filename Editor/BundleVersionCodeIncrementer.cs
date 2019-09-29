1d5e753e5fba093e75d8e0385a30074080b47740
baba_s <shotababa1206@gmail.com>
2019-09-29 15:59:25 +0900

    first commit

---
+++ Editor/BundleVersionCodeIncrementer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+namespace KoganeUnityLib
+{
+	internal sealed class BundleVersionCodeIncrementer : IPostprocessBuildWithReport
+	{
+		public int callbackOrder => 0;
+
+		public void OnPostprocessBuild( BuildReport report )
+		{
+			var platform = report.summary.platform;
+
+			switch ( platform )
+			{
+				case BuildTarget.iOS:
+					var number = int.Parse( PlayerSettings.iOS.buildNumber ) + 1;
+					PlayerSettings.iOS.buildNumber = number.ToString();
+					break;
+
+				case BuildTarget.Android:
+					var code = PlayerSettings.Android.bundleVersionCode + 1;
+					PlayerSettings.Android.bundleVersionCode = code;
+					break;
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Uni Bundle Version Code Incrementer
+
+iOS / Android ビルド完了時に Bundle Version Code や Bundle Number を 1 増やすエディタ拡張
+
+![](https://img.shields.io/badge/Unity-2018.4%2B-red.svg)
+![](https://img.shields.io/badge/.NET-4.x-orange.svg)
+[![](https://img.shields.io/github/license/baba-s/uni-bundle-version-code-incrementer.svg)](https://github.com/baba-s/uni-bundle-version-code-incrementer/blob/master/LICENSE)
+
+## インストール
+
+```json
+"com.baba_s.uni-bundle-version-code-incrementer": "https://github.com/baba-s/uni-bundle-version-code-incrementer.git",
+```
+
+manifest.json に上記の記述を追加します  
